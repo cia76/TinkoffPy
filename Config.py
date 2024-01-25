cafbb2e0ba80c1d7ac076702fb9a17910927b409
cia76 <ichechet@mail.ru>
2024-01-26 01:59:30 +0500

    Шаблон

--- Config.py
+++ Config.py
@@ -1,5 +1,5 @@
 # Получить токен можно в настройках профиля Тинькофф Инвестиции по ссылке: https://www.tinkoff.ru/invest/settings/
 
 class Config:
-    AccountIds = (2012064636,)  # Торговые счета
-    Token = 't.XY2YS5MxkVJ2_TQQ5g-0dvZLn4e66xiW5q7O32DUyUMXsFXUXqU1YObMlWfXDRwEFsaC-QO9EvKG-sRu3gvQtQ'
+    AccountIds = (1234567890,)  # Торговые счета
+    Token = '<Токен>'
